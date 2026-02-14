8b31d280f063a890b03cc1b786cc956c3be2ac84
Prashant K Singh <prashantkumarsingh934@gmail.com>
2026-02-14 19:47:44 +0530

    added one more endpoint /about

--- main.py
+++ main.py
@@ -2,7 +2,10 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-@app.get("/")
+@app.get('/')
 def hello():
     return{ 'message':"Hello World !!"}
-    
+
+@app.get('/about')
+def about():
+    return {'message': 'This is a hello world website'}
