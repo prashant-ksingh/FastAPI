ed82dbb9896b3b6af8ab5145504306be2362616b
Prashant K Singh <prashantkumarsingh934@gmail.com>
2026-02-18 07:00:49 +0530

    path paremeter : Endpoint to view specific patient data

--- main.py
+++ main.py
@@ -1,28 +1,36 @@
-from fastapi import FastAPI
 import json
+
+from fastapi import FastAPI
 
 app = FastAPI()
 
+
 def load_data():
-    with open('patients.json', 'r') as f:
+    with open("patients.json", "r") as f:
         data = json.load(f)
     return data
 
-@app.get('/')
+
+@app.get("/")
 def hello():
-    return{ 'message':"Patient Management System API"}
+    return {"message": "Patient Management System API"}
 
-@app.get('/about')
+
+@app.get("/about")
 def about():
-    return {'message': 'Fully Functiuonal API to manage patient records'}
+    return {"message": "Fully Functiuonal API to manage patient records"}
 
 
-@app.get('/view')
+@app.get("/view")
 def view():
     data = load_data()
     return data
 
-    
 
+@app.get("/patient/{patient_id}")
+def view_patient(patient_id: str):
+    data = load_data()
+    if patient_id in data:
+        return data[patient_id]
 
-
+    return {"error": "patient not found !!"}
