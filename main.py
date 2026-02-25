bcfcfe9bbbb19698d8bc0489ea0f77e214cd9a93
Prashant K Singh <prashantkumarsingh934@gmail.com>
2026-02-25 23:40:24 +0530

    Update and Delete Api

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import json
 from fastapi import FastAPI, HTTPException, Path, Query
 from fastapi.responses import JSONResponse
 from pydantic import BaseModel, Field, computed_field
-from typing import Annotated, Literal
+from typing import Annotated, Literal, Optional
 
 app = FastAPI()
 
@@ -34,6 +34,13 @@ class Patient(BaseModel):
         else:
             return 'Obese'
 
+class PatientUpdate(BaseModel):
+    name: Annotated[Optional[str], Field(default=None)]
+    city: Annotated[Optional[str], Field(default=None)]
+    age: Annotated[Optional[int], Field(default=None, gt=0)]
+    gender: Annotated[Optional[Literal['male', 'female']], Field(default=None)]
+    height: Annotated[Optional[float], Field(default=None, gt=0)]
+    weight: Annotated[Optional[float], Field(default=None, gt=0)]
 
 
 
@@ -111,4 +118,47 @@ def create_patient(patient: Patient):
     save_data(data)
 
     return JSONResponse(status_code=201, content={'message':'patient created sucessfully'})
+
+
+
+@app.put('/edit/{patient_id}')
+def update_patient(patient_id: str, patient_update: PatientUpdate):
+    data = load_data()
+
+    if patient_id not in data:
+        raise HTTPException(status_code=404, detail='Patient not found')
+    
+    existing_patient_info = data[patient_id]
+
+    updated_patient_info = patient_update.model_dump(exclude_unset= True)     #exclude_unset = True will set only the values recived to  update
+    
+    for key, value in updated_patient_info.items():
+        existing_patient_info[key] = value
+
+    # existing_patient_info -> pydantic object -> updated bmi + verdict
+    existing_patient_info['id'] = patient_id
+    patient_pydantic_obj = Patient(**existing_patient_info) 
+    # -> pydantic object -> dict
+    existing_patient_info = patient_pydantic_obj.model_dump(exclude='id')
+   
+    # add this dict to data
+    data[patient_id] = existing_patient_info
+
+    # save data
+    save_data(data)
+    return JSONResponse(status_code=200, content={'message':'patient updated'}) 
+
+@app.delete('/delete/{patient_id}')
+def delete_patient(patient_id: str):
+
+    # load data
+    data = load_data()
+
+    if patient_id not in data:
+        raise HTTPException(status_code=404, detail='Patient not found')
+    
+    del data[patient_id]
+
+    save_data(data)
+    return JSONResponse(status_code=200, content={'message': "patient Deleted"})
 
